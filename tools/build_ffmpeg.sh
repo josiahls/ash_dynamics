e5743489f4f85252eac0ca4b2a16644cfe1ab642
josiah <jklaivins@gmail.com>
2025-12-02 02:10:26 +0000

    feat: Add tool to build ffmpeg script that builds the smae components are opencv

---
+++ tools/build_ffmpeg.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Follow how opencv links to ffmpeg: 
+# https://github.com/opencv/opencv/blob/a08565253ee098eae55e893d3f8e5d9a011440a9/CMakeLists.txt#L1631
+# and
+# https://github.com/microsoft/vcpkg/issues/19334
+# Install / configure only these packages:
+# "avcodec"
+# "avformat"
+# "swresample"
+# "swscale"
+
+cd ./third_party/ffmpeg
+
+# For put the built dir in the same directory. Eventually change to the default
+# location on linux.
+./configure \
+    --prefix=./build \
+    --enable-shared \
+    --disable-avdevice \
+    --disable-avfilter \
+    --disable-pthreads \
+    --disable-w32threads \
+    --disable-os2threads \
+    --disable-network \
+    --disable-dwt \
+    --disable-error-resilience \
+    --disable-lsp \
+    --disable-faan \
+    --disable-iamf \
+    --disable-pixelutils
+    # We want to leave these enabled.
+    # --disable-avcodec 
+    # --disable-avformat
+    # --disable-swresample
+    # --disable-swscale
+
+make
+make install
