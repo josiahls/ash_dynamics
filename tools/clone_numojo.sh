c17efbdd4ae994ea5958ef9f72ea7d3f44813cbb
josiah <jklaivins@gmail.com>
2026-01-17 19:25:15 +0000

    fix: Switch NuMojo from pre-0.9 to feature/ndarray-buffer-support

--- tools/clone_numojo.sh
+++ tools/clone_numojo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -d third_party/NuMojo ]; then 
-    git clone https://github.com/josiahls/NuMojo.git third_party/NuMojo --branch pre-0.9 --single-branch
+    git clone https://github.com/josiahls/NuMojo.git third_party/NuMojo --branch feature/ndarray-buffer-support --single-branch
 else 
     echo 'NuMojo directory already exists.'
 fi
